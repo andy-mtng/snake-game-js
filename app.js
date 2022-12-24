a5210fb8107049ce00cf34a9f287c0b88e15a7ff
Andy N <andynguyen9001@gmail.com>
2022-12-23 21:17:47 -0700

    Add food functionality

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 document.addEventListener('keydown', handleKeyboardEvents);
-// window.setInterval(moveSnake, 150);
+window.setInterval(moveSnake, 150);
 
 const snake = document.querySelector(".snake");
+const food = document.querySelector(".food");
+
+console.log(snake);
+console.log(food);
+
 const gameboardHeight = 500;
 const gameboardWidth = 700;
 const movementDistance = 20; 
-const singleSnakeSize = 20;
-let marginLeft = 0;
-let marginTop = 0;
+const playerBlockSize = 20;
+
+let marginLeft = 200;
+let marginTop = 100;
+
+let foodMarginLeft = 500;
+let foodMarginTop = 200;
+
 let movementDirection = "right";
 
 
 function handleKeyboardEvents(event) {
     let code = event.code;
-    console.log("code = ", code);
-
     // Prevent user from moving the browser window with the arrow keys or space
     if (["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(code) > -1) {
         event.preventDefault();
@@ -35,7 +43,6 @@ function handleKeyboardEvents(event) {
 function moveSnake() {
     if (movementDirection == "right") {
         marginLeft += movementDistance;
-        console.log(marginLeft + "px");
         snake.style.marginLeft = marginLeft + "px";
     } else if (movementDirection == "left") {
         marginLeft -= movementDistance;
@@ -47,14 +54,62 @@ function moveSnake() {
         marginTop += movementDistance;
         snake.style.marginTop = marginTop + "px";   
     }
-    detectBorderHit()
+    detectBorderHit();
+    detectFoodEaten();
 }
 
 
 function detectBorderHit() {
-    if ((marginLeft === (-movementDistance || gameboardWidth + movementDistance)) || 
-        (marginTop === (-movementDistance || gameboardHeight + movementDistance))) {
-            alert("Border hit");
-        } 
+    // Snake hit the left or top border of the game 
+    if (marginLeft === -movementDistance || marginTop === -movementDistance) {
+        alert("Border hit");
+        resetSnakePosition();
+    // Snake hit the right or bottom border of the game
+    } else if (marginLeft === gameboardWidth || marginTop === gameboardHeight) {
+        alert("Border hit");
+        resetSnakePosition();
+    }
 }
 
+
+function detectFoodEaten() {
+    if (marginLeft === foodMarginLeft && marginTop === foodMarginTop) {
+        alert("Food eaten.");
+        createNewFood();
+    }
+}
+
+
+function createNewFood() {
+    const possMarginLeft = [];
+    const possMarginTop = [];
+
+    for (let i = 0; i <= (gameboardWidth - playerBlockSize); i += 20) {
+        possMarginLeft.push(i);
+    }
+
+    for (let i = 0; i <= (gameboardHeight - playerBlockSize); i += 20) {
+        possMarginTop.push(i);
+    }
+
+    // Generate random number between 0 and 680 (at 20px increments)
+    foodMarginLeft = possMarginLeft[Math.floor(Math.random() * possMarginLeft.length)]
+    // Generate random number between 0 and 480 (at 20px increments)
+    foodMarginTop = possMarginTop[Math.floor(Math.random() * possMarginTop.length)]
+    console.log(foodMarginLeft, foodMarginTop);
+
+    food.style.marginLeft = foodMarginLeft + "px";
+    food.style.marginTop = foodMarginTop + "px";
+    console.log(food);
+}
+
+
+function resetSnakePosition() {
+    marginLeft = 200;
+    marginTop = 100;
+
+    snake.style.marginLeft = marginLeft + "px";
+    snake.style.marginTop = marginTop + "px";
+}
+
+ 
